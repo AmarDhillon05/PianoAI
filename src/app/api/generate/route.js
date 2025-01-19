bbcac628fbfbf2d82bc56c76b5cad48fad1859ab
Amar Dhillon <87444806+AmarDhillon05@users.noreply.github.com>
2025-01-19 16:00:50 -0500

    Update route.js

--- src/app/api/generate/route.js
+++ src/app/api/generate/route.js
@@ -7,7 +7,7 @@ export async function POST(req) {
     const notes = body.notes
 
     //Requesting from ec2
-    const resp = await fetch('http://54.173.8.41/', {method: "POST", body: JSON.stringify(notes)})
+    const resp = await fetch(process.env.URL, {method: "POST", body: JSON.stringify(notes)})
     const json_resp = await resp.json()
     return new Response(JSON.stringify(json_resp))
     
